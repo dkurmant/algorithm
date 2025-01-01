f7a236d4ea177c851457ece76899bcd695a272ed
DidarKur <didarak179@gmail.com>
2025-01-02 01:45:59 +0500

    solve 1.1 problem in informatics

---
+++ informatics/1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a,b,res; 
+    cin >> a;
+    cin >> b;
+    cout << res;
+    res = sqrt(pow(a,2) + pow(b,2));
+    return 0;
+}
